269dcd447518b47e2fa956cfb5a35f0eb6518750
出町史夏 <demachifumika@Demachi-MacbookAir.local>
2022-09-27 18:15:34 +0900

    #7 Add beam squint scripts

---
+++ nercst/core/io_extension.py
@@ -0,0 +1,184 @@
+from pathlib import Path
+from datetime import datetime
+
+import numpy as np
+import xarray as xr
+import necstdb
+
+from nasco_analysis import io
+
+
+def list_all_topics(path):
+    """List file names which necstdb can read."""
+    p = Path(path).glob("**/*")
+    return sorted(set([a.stem for a in p if a.is_file()]))
+
+
+def get_tp_data(path, topic_name, kisa_path=None, weather=False):
+    """TotalPower data with numerous metadata.
+
+    This function supports data from AC240 and `concatenate` without
+    *kisa* file. Weather informations will be with the TP data if you
+    set the option `weather=True`.
+
+    Examples
+    --------
+    >>> path = '/mnt/usbdisk9/data/observation/otf_planet2018/n20200320131924_12CO_2-1_otfplanet_sun'
+    >>> topic_name = ['xffts_power_board01', 'ac240_tp_data_1']
+    >>> get_tp_data(path, topic_name[0])
+    "<xarray.DataArray (t: 1191)>\narray([2.25653151e+11, ..."
+
+    Notes
+    -----
+    [TODO] Support multiple topics, maybe by wrapping this function.
+    """  # noqa: E501
+    data = io.Initial_array(path, kisa_path, topic_name)
+    dat, obsmode, *_ = data.get_tp_array()  # `_` is for value(s) to be ignored
+    # also cf. extended unpacking
+
+    # convert structured array to xarray
+    coords = {"t": dat.t.astype(np.float64)}
+    for name in dat.data.dtype.names:
+        coords[name] = ("t", dat.data[name].astype(np.float64))
+    # extract main data
+    if "total_power" in dat.data.dtype.names:
+        tp_data = coords.pop("total_power")[1]
+    elif "POWER_BE1" in dat.data.dtype.names:
+        tp_data = coords.pop("POWER_BE1")[1]
+    else:
+        raise ValueError("Total power field not found")
+    # timestamp is not needed anymore
+    coords.pop("timestamp")
+    # make DataArray
+    data_array = xr.DataArray(
+        tp_data,
+        dims=["t"],
+        coords=coords,
+    )
+
+    # perform or escape `apply_kisa`
+    if kisa_path:
+        data.apply_kisa()
+    else:
+        data.kisa_applyed_az = data.az_array  # fooling instance attribute(s)
+        data.kisa_applyed_el = data.el_array  # is not a good way...
+
+    # create weather informations coordinates
+    if weather:
+        db = necstdb.opendb(path)
+        weather = db.open_table("status_weather").read(astype="array")
+        contents = {"t": weather["timestamp"].astype(np.float64)}
+        for name in weather.dtype.names:
+            contents[name] = ("t", weather[name].astype(np.float64))
+        contents.pop("timestamp")
+        contents.pop("received_time")  # due to conflict with `received_time` of TP data
+        # apply the coordinates
+        weather = xr.Dataset(contents)
+        reindexed_weather = weather.interp_like(data_array)
+        coords = {}
+        for key in reindexed_weather.variables.keys():
+            coords[key] = ("t", reindexed_weather[key])
+        data_array = data_array.assign_coords(**coords)
+
+    # list coordinates of constant values (possibly incorrect values) or `nan` only
+    unique = []
+    for name in data_array.coords.keys():
+        elem = np.unique(data_array[name])  # `nan`s are not compressed
+        if len(elem[~np.isnan(elem)]) <= 1:  # len(`nan`s removed), tilde is bitwise NOT
+            unique.append(name)
+    data.data_array = data_array.assign_attrs({"constant_or_nodata": unique})
+
+    # convert timestamp to datetime obj, making `utcfromtimestamp` accept iterable
+    ret = data.concatenate()
+    # set descriptive string instead of digits
+    ret = ret.assign_coords(
+        {
+            "t": np.vectorize(datetime.utcfromtimestamp)(ret.t),
+            "obsmode": ("t", obsmode.reindex_like(ret, "nearest")),
+        }
+    )
+    return ret
+
+
+def get_spec_data(path, topic_name, kisa_path=None, weather=False):
+    """Spectral data with numerous metadata.
+
+    This function supports data from AC240 and `concatenate` without
+    *kisa* file. Weather informations will be with the data if you
+    set the option `weather=True`.
+
+    Examples
+    --------
+    >>> path = '/mnt/usbdisk9/data/observation/otf/otf_2019/n20200320193251_12CO_2-1_otf_OriKL'
+    >>> topic_name = ['xffts_board01', 'ac240_spactra_data_1']
+    >>> kisa_path = '/home/amigos/seigyo/backup/hosei/hosei_230.txt'
+    >>> get_spec_data(path, topic_name[0], kisa_path, True)
+    "<xarray.DataArray (t: 20934, spec: 32768)>\narray([[1.28365281e+10, ..."
+
+    Notes
+    -----
+    [TODO] Support multiple topics, maybe by wrapping this function.
+    """  # noqa: E501
+    data = io.Initial_array(path, kisa_path, topic_name)
+    dat, obsmode, *_ = data.get_tp_array()  # not a bug, this is a WORKAROUND
+
+    # convert structured array to xarray
+    coords = {"t": dat.t.astype(np.float64)}
+    for name in dat.data.dtype.names:
+        coords[name] = ("t", dat.data[name].astype(np.float64))
+    # extract main data
+    if "spec" in dat.data.dtype.names:
+        tp_data = coords.pop("spec")[1]
+    else:
+        raise ValueError("Spectral data field not found")
+    # timestamp is not needed anymore
+    coords.pop("timestamp")
+    # make DataArray
+    data_array = xr.DataArray(
+        tp_data,
+        dims=["t", "spec"],
+        coords=coords,
+    )
+
+    # perform or escape `apply_kisa`
+    if kisa_path:
+        data.apply_kisa()
+    else:
+        data.kisa_applyed_az = data.az_array  # fooling instance attribute(s)
+        data.kisa_applyed_el = data.el_array  # is not a good way...
+
+    # create weather informations coordinates
+    if weather:
+        db = necstdb.opendb(path)
+        weather = db.open_table("status_weather").read(astype="array")
+        contents = {"t": weather["timestamp"].astype(np.float64)}
+        for name in weather.dtype.names:
+            contents[name] = ("t", weather[name].astype(np.float64))
+        contents.pop("timestamp")
+        contents.pop("received_time")  # due to conflict with `received_time` of TP data
+        # apply the coordinates
+        weather = xr.Dataset(contents)
+        reindexed_weather = weather.interp_like(data_array)
+        coords = {}
+        for key in reindexed_weather.variables.keys():
+            coords[key] = ("t", reindexed_weather[key])
+        data_array = data_array.assign_coords(**coords)
+
+    # list coordinates of constant values (possibly incorrect values) or `nan` only
+    unique = []
+    for name in data_array.coords.keys():
+        elem = np.unique(data_array[name])  # `nan`s are not compressed
+        if len(elem[~np.isnan(elem)]) <= 1:  # len(`nan`s removed), tilde is bitwise NOT
+            unique.append(name)
+    data.data_array = data_array.assign_attrs({"constant_or_nodata": unique})
+
+    # convert timestamp to datetime obj, making `utcfromtimestamp` accept iterable
+    ret = data.concatenate()
+    # set descriptive string instead of digits
+    ret = ret.assign_coords(
+        {
+            "t": np.vectorize(datetime.utcfromtimestamp)(ret.t),
+            "obsmode": ("t", obsmode.reindex_like(ret, "nearest")),
+        }
+    )
+    return ret
---
+++ nercst/sunscan/sun_scan_chopper.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import pickle
+
+from astropy.coordinates import get_body, AltAz
+from astropy.time import Time
+import astropy.constants as const
+import astropy.units as u
+import xarray as xr
+import numpy as np
+
+# import constants as n2const
+import n_const.constants as n2const
+from io_extension import get_tp_data
+
+
+class ChopperSun:
+    TOPIC_PREFIX = {
+        "100G": np.array(["xffts_power_board"]),
+        "200G": np.array(["ac240_tp_data_"]),
+    }
+
+    def __init__(
+        self, path_to_data, path_to_kisa, topic_name, body="sun", obs_freq=None
+    ):
+        if not obs_freq:
+            self.obs_freq = self.board2beam(topic_name)
+        else:
+            self.obs_freq = obs_freq
+        self.body = body
+        self.data = get_tp_data(
+            path_to_data, topic_name, path_to_kisa, weather=True
+        ).assign_attrs({"obs_freq": self.obs_freq})
+        return
+
+    def get_target_coord(self):
+        target = get_body(body=self.body, time=Time(self.data.t))
+        args = {
+            "obstime": Time(self.data.t),
+            "pressure": self.data["press"].mean().data * u.hPa,
+            # "temperature": (self.data["out_temp"].mean().data * u.K).to(
+            #     u.deg_C, equivalencies=u.equivalencies.temperature()
+            # ),
+            # "relative_humidity": self.data["out_humi"].mean().data * u.percent,
+            "temperature": 15 * u.deg_C,  # TODO: give as kwargs?
+            "relative_humidity": 20 * u.percent,  # TODO: give as kwargs?
+            "location": n2const.LOC_NANTEN2,
+        }
+        if self.obs_freq == "100G":
+            obswl = (const.c / (n2const.REST_FREQ.j10_12co)).to("micron")
+        elif self.obs_freq == "200G":
+            obswl = (const.c / (n2const.REST_FREQ.j21_12co)).to("micron")
+        else:
+            raise ValueError("`obs_freq` must be '100G' or '200G'")
+        self.target = target.transform_to(AltAz(obswl=obswl, **args,))
+        return self.target
+
+    def get_relative_position(self):
+        """
+        Relative position from center of the target.
+
+        Notes
+        -----
+        Needed in ``sun_scan_beampattern``.
+        """
+        self.get_target_coord()
+        # relative coordinate from center of the target
+        res = {
+            "resAz": self.target.az.deg - self.data["azlist"],
+            "resEl": self.target.alt.deg - self.data["ellist"],
+        }
+        self.data = self.data.assign_coords(res)
+        return
+
+    @classmethod
+    def board2beam(cls, topic_name):
+        """
+        Guess observation frequency from topic name.
+        """
+        for beam, spectrometer in cls.TOPIC_PREFIX.items():
+            if topic_name.startswith(spectrometer[0]):
+                return beam
+        raise ValueError("Unknown topic name")
+
+    def chopper_wheel(self):
+        self.get_relative_position()
+        tp_hot_mean, tp_off_mean, *_ = self.data.groupby("obsmode").mean(
+            keep_attrs=True
+        )
+        tp_on = self.data.where(self.data.obsmode == 2, drop=True)
+        with xr.set_options(keep_attrs=True):
+            # Ta* = [P(ON) - P(OFF)] / [P(HOT) - P(OFF)] * Tamb
+            t_a = (tp_on - tp_off_mean) / (tp_hot_mean - tp_off_mean) * 300
+        return t_a
+
+
+IF_LIST = {
+    "100G": np.array(
+        [
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+        ]
+    ),
+    "200G": np.array(["1", "2", "3", "4"]),
+}
+
+TP_TOPICS = np.concatenate(
+    (
+        np.char.add(ChopperSun.TOPIC_PREFIX["100G"], IF_LIST["100G"]),
+        np.char.add(ChopperSun.TOPIC_PREFIX["200G"], IF_LIST["200G"]),
+    )
+)  # ac240_tp_data_1, ac240_tp_data_2, xffts_power_board01, ..., xffts_power_board16
+
+
+def create_chopperwheeled_data(
+    path_to_data,
+    path_to_kisa,
+    save_dir="./",
+    topic_name=TP_TOPICS,
+    body="sun",
+    obs_freq=None,
+):
+    """
+    Notes
+    -----
+    Wall time: 30s / 18IF
+    """
+    # path_to_data = Path(path_to_data)
+    save_dir = Path(save_dir).absolute()
+    if path_to_data.name.find(body) == -1:  # filename doesn't contain target name
+        return
+    # loop through IFs
+    t_a = {}
+    if isinstance(topic_name, str):
+        topic_name = [topic_name]
+    for topic in topic_name:
+        try:
+            cs = ChopperSun(path_to_data, path_to_kisa, topic, body, obs_freq)
+            t_a[topic] = cs.chopper_wheel()
+        except KeyError:  # catch error from specifying all 20IF
+            t_a[topic] = None
+    # save
+    filename = (save_dir / path_to_data.name).with_suffix(".pickle")
+    print(f"Data file is at {filename}")
+    with open(filename, "wb") as f:
+        pickle.dump(t_a, f)
+    return filename
+
+
+if __name__ == "__main__":
+    pass
