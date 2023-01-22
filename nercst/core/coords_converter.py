4f42fb36f30cc9d27579d1f06a2b4259a3ca2360
r-yamada1998 <yamadier@icloud.com>
2023-01-22 19:43:56 +0900

    "#45 coordinate converter implemented"

---
+++ nercst/core/coords_converter.py
@@ -0,0 +1,24 @@
+import astropy.units as u
+import xarray as xr
+
+from neclib import config
+from astropy.time import Time
+from neclib.coordinates import parse_frame
+from astropy.coordinates import SkyCoord
+from neclib.parameters import PointingError
+
+
+def add_celestial_coords(array: xr.DataArray, frame: str) -> xr.DataArray:
+
+    pe = PointingError("OMU1P85M")
+    lon, lat = pe.apparent2refracted(
+        array["lon"].values * u.deg, array["lat"].values * u.deg
+    )
+    target_frame = parse_frame(frame)
+    obstime = Time(array.t, format="unix")
+    lon_lat = SkyCoord(
+        lon, lat, frame="altaz", obstime=obstime, location=config.location
+    ).transform_to(target_frame)
+    array = array.assign_coords({"lon_cor": ("t", lon_lat.lon.value)})
+    array = array.assign_coords({"lat_cor": ("t", lon_lat.lat.value)})
+    return array
