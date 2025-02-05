08297c33eafc2991148f2726c22103a53bb13629
Fumika Demachi <f.demachi@a.phys.nagoya-u.ac.jp>
2025-02-05 11:22:54 +0900

    #83 add analysis_params

---
+++ nercst/core/analysis_params.py
@@ -0,0 +1,11 @@
+from astropy import units as u
+
+rest_frequency = {
+    "12CO(1-0)": 115.271202 * u.GHz,
+    "12CO(2-1)": 230.538000 * u.GHz,
+    "13CO(1-0)": 110.201353 * u.GHz,
+    "13CO(2-1)": 220.398681 * u.GHz,
+    "C18O(1-0)": 109.782173 * u.GHz,
+    "C18O(2-1)": 219.560354 * u.GHz,
+    "SO": 219.940000 * u.GHz,
+}
